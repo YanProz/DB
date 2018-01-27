17b11633149d0fa9f57c979f152f70dd432bb8f6
YanaProz <yanaprozorova753@gmail.com>
2018-01-27 12:25:08 +0300

    last version

---
+++ app.js
@@ -0,0 +1,23 @@
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var express = require('express');
+var bcrypt = require('bcryptjs');
+var csrf = require('csurf');
+
+
+// MODELS
+
+Admin =require('./models/admin');
+Pupil =require('./models/pupil');
+Event =require('./models/event');
+//
+
+var app = express();
+app.locals.pretty = true;
+app.use(bodyParser.urlencoded({extended:true}));
+
+mongoose.connect('mongodb://localhost:27017/DBManager');
+app.use(require('./api'));
+
+app.listen(3000);
+console.log("listening 3000");
