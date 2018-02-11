302793ad48fb4686d0b1443360a7de969e12e910
YanaProz <yanaprozorova753@gmail.com>
2018-02-11 19:27:49 +0300

    modified

--- app.js
+++ app.js
@@ -12,12 +12,21 @@ Pupil =require('./models/pupil');
 Event =require('./models/event');
 //
 
+index_api = require('./public/routes/index');
+cabinet_api = require('./public/routes/cabinet');
+add_item_api = require('./public/routes/add-item');
+add_event_api = require('./public/routes/add-event');
+
 var app = express();
 app.locals.pretty = true;
 app.use(bodyParser.urlencoded({extended:true}));
 
 mongoose.connect('mongodb://localhost:27017/DBManager');
-app.use(require('./api'));
+
+app.use('/', index_api);
+app.use('/cabinet', cabinet_api);
+app.use('/add-item', add_item_api);
+app.use('/add-event', add_event_api);
 
 app.listen(3000);
 console.log("listening 3000");
